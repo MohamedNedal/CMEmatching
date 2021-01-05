607c55a1730a7b864bb7dba3cfb8a7fb9601bb20
MohamedNedal <mohamed.nedal.solar@gmail.com>
2021-01-06 01:24:08 +0200

    Still has an unlogical issue

--- CME_ICME_Match.py
+++ CME_ICME_Match.py
@@ -89,41 +89,47 @@ for event_num in range(len(sample)):
     # Convert the collection of index labels to list 
     Index_label_Dst = omni_data[omni_data['DST1800'] <= threshold].index.tolist()
     
-    print('Define timestamps where Dst =< ', round(threshold,2), 'nT')
+    if Index_label_Dst == []:
+        print('-------------------------------------------------------\n')
+        print('Index_label_Dst is empty')
+        print('-------------------------------------------------------\n')
     
-    if min(omni_data['DST1800']) <= threshold:
-        
-        fig, axs = plt.subplots(8, 1, figsize=(15,15), dpi=300, sharex=True)
-        
-        axs[0].plot(omni_data['F1800'], color='blue', label='$B_t$')
-        axs[0].plot(omni_data['BZ_GSE1800'], color='red', label='$B_z$')
-        axs[0].set_ylabel('B (nT)')
-        
-        axs[1].plot(omni_data['THETA_AV1800'], label=r'$\theta$')
-        axs[1].plot(omni_data['PHI_AV1800'], label=r'$\phi$')
-        axs[1].set_ylabel('Angle (deg)')
-        
-        axs[2].plot(omni_data['BX_GSE1800'], label=r'$B_x$ $GSE$')
-        axs[2].plot(omni_data['BY_GSE1800'], label=r'$B_y$ $GSE$')
-        axs[2].set_ylabel('B (nT)')
-        
-        axs[3].plot(omni_data['V1800'], label='Flow Speed')
-        axs[3].set_ylabel(r'$V$ $(km.s^{-1})$')
-        
-        axs[4].plot(omni_data['N1800'], label='Proton Density')
-        axs[4].set_ylabel(r'$n_{p}$ $(cm^{-3})$')
-        
-        axs[5].plot(omni_data['Pressure1800'], label='Dynamic Pressure')
-        axs[5].set_ylabel('P (nPa)')
-        
-        axs[6].plot(omni_data['Beta1800'], label='Plasma Beta')
-        axs[6].set_ylabel('Beta')
-        
-        axs[7].plot(omni_data['DST1800'], label='Dst index')
-        axs[7].set_ylabel('Dst (nT)')
-        
-        if Index_label_Dst != []:
+    else:
+        print('Define timestamps where Dst =< ', round(threshold,2), 'nT')
+    
+        if min(omni_data['DST1800']) <= threshold:
             
+            fig, axs = plt.subplots(8, 1, figsize=(15,15), dpi=300, sharex=True)
+            
+            fig.suptitle('For CME event that is launched on: '+str(sample.index[event_num]))
+            
+            axs[0].plot(omni_data['F1800'], color='blue', label='$B_t$')
+            axs[0].plot(omni_data['BZ_GSE1800'], color='red', label='$B_z$')
+            axs[0].set_ylabel('B (nT)')
+            
+            axs[1].plot(omni_data['THETA_AV1800'], label=r'$\theta$')
+            axs[1].plot(omni_data['PHI_AV1800'], label=r'$\phi$')
+            axs[1].set_ylabel('Angle (deg)')
+            
+            axs[2].plot(omni_data['BX_GSE1800'], label=r'$B_x$ $GSE$')
+            axs[2].plot(omni_data['BY_GSE1800'], label=r'$B_y$ $GSE$')
+            axs[2].set_ylabel('B (nT)')
+            
+            axs[3].plot(omni_data['V1800'], label='Flow Speed')
+            axs[3].set_ylabel(r'$V$ $(km.s^{-1})$')
+            
+            axs[4].plot(omni_data['N1800'], label='Proton Density')
+            axs[4].set_ylabel(r'$n_{p}$ $(cm^{-3})$')
+            
+            axs[5].plot(omni_data['Pressure1800'], label='Dynamic Pressure')
+            axs[5].set_ylabel('P (nPa)')
+            
+            axs[6].plot(omni_data['Beta1800'], label='Plasma Beta')
+            axs[6].set_ylabel('Beta')
+            
+            axs[7].plot(omni_data['DST1800'], label='Dst index')
+            axs[7].set_ylabel('Dst (nT)')
+
             # Find Geomagnetic Storms in Data 
             for i in range(1, len(omni_data)):
                 
@@ -137,7 +143,7 @@ for event_num in range(len(sample)):
                     axs[5].axvspan(omni_data['Pressure1800'].index[i-1], omni_data['Pressure1800'].index[i], facecolor='#FFCC66', alpha=0.5)
                     axs[6].axvspan(omni_data['Beta1800'].index[i-1], omni_data['Beta1800'].index[i], facecolor='#FFCC66', alpha=0.5)
                     axs[7].axvspan(omni_data['DST1800'].index[i-1], omni_data['DST1800'].index[i], facecolor='#FFCC66', alpha=0.5)                    
-
+    
                     ''' 
                      RUN A FOR-LOOP THAT CHECK THE MIN. DIFF. BETWEEN 
                      'arrival_datetime' & 'Index_label_<ANY_SW_PARAM.>' 
@@ -150,67 +156,58 @@ for event_num in range(len(sample)):
                     for ax in axs:
                         # estimated travel time from G2001 model 
                         ax.axvline(arrival_datetime, label='G2001', color='green', alpha=0.7, linewidth=3, linestyle='--')
-                        
-                        # Find the local min Dst value within the window of 'Index_label_Dst' 
-                        try: 
-                            if Index_label_Dst == []:
-                                print('-------------------------------------------------------\n')
-                                print('Index_label_Dst is empty')
-                                print('-------------------------------------------------------\n')
-                                
-                            else:            
-                                min_Dst_window = min(omni_data['DST1800'].loc[Index_label_Dst[0]:Index_label_Dst[-1]])
-                                ax.axvline(omni_data[omni_data['DST1800']==min_Dst_window].index[0], label='Min(Dst)', color='red', alpha=0.7, linewidth=3, linestyle='--')
-                                
-                                dt_G2001_idxLabel = []
-                                for idx in range(len(Index_label_Dst)):
-                                    dt_G2001_idxLabel.append(abs(arrival_datetime - Index_label_Dst[idx]))
-                                
-                                # at the index of the min diff betn t_G2001 and t_min(Dst) within the window 
-                                ax.axvline(Index_label_Dst[dt_G2001_idxLabel.index(min(dt_G2001_idxLabel))], label='Min(dt)', color='black', alpha=0.7, linewidth=3, linestyle='--')     
-                        
-                                T_RED = omni_data[omni_data['DST1800']==min_Dst_window].index[0]
-                                T_BLACK = Index_label_Dst[dt_G2001_idxLabel.index(min(dt_G2001_idxLabel))]
-                                T_GREEN = arrival_datetime
-                                
-                                AVG = Timestamp((T_RED.value + T_BLACK.value + T_GREEN.value)/3.0)
-                                
-                                ax.axvline(AVG, label='AVG_t', color='brown', alpha=0.7, linewidth=3, linestyle='--')
-                                
-                                ax.legend(loc='upper right', frameon=False, prop={'size': 10})
-                                ax.set_xlim([omni_data.index[0], omni_data.index[-1]])
-                                
-                                plt.xlabel('Date')
-                                fig.tight_layout()
-                                
-                                st = str(omni_data.index[0].year)+str(omni_data.index[0].month)+str(omni_data.index[0].day)+str(omni_data.index[0].hour)+str(omni_data.index[0].minute)+str(omni_data.index[0].second)
-                                en = str(omni_data.index[-1].year)+str(omni_data.index[-1].month)+str(omni_data.index[-1].day)+str(omni_data.index[-1].hour)+str(omni_data.index[-1].minute)+str(omni_data.index[-1].second)
-                                plt.savefig(os.path.join(save_path, 'Output_plots' + '/', 'OMNI_Data_for_CME_No_'+str(event_num)+'_'+st+'-'+en+'.png'))
-                                                    
-                                # APPEND THE OUTPUT TRANSIT TIME WITH THE CME INFO 
-                                est_trans_time = Index_label_Dst[dt_G2001_idxLabel.index(min(dt_G2001_idxLabel))] - sample.index[event_num]
-                                
-                                tran_time_hours = (est_trans_time.components.days * 24) + (est_trans_time.components.minutes / 60) + (est_trans_time.components.seconds / 3600)
-                                
-                                final_table = final_table.append({'CME_datetime': sample.index[event_num], 
-                                                                  'Width': sample['Width'][event_num], 
-                                                                  'Linear_Speed': sample['Linear_Speed'][event_num], 
-                                                                  'Initial_Speed': sample['Initial_Speed'][event_num], 
-                                                                  'Final_Speed': sample['Final_Speed'][event_num], 
-                                                                  'Speed_20Rs': sample['Speed_20Rs'][event_num], 
-                                                                  'Accel': sample['Accel'][event_num], 
-                                                                  'MPA': sample['MPA'][event_num], 
-                                                                  'Transit_time_hrs': tran_time_hours, 
-                                                                  'est_ICME_datetime': est_trans_time}, ignore_index=True)
-                                
-                                Index_label_Dst, min_Dst_window = [], []
-                    
-                        except IndexError as idx_err:
-                            print(idx_err)
 
-    else:
-        print('The OMNI data from ' + str(start_datetime) + ' to '+ str(end_datetime) + ' has no Dst value below ' + str(threshold) + ' nT.')
-        print('-------------------------------------------------------\n')
+                        # Find the local min Dst value within the window of 'Index_label_Dst'           
+                        min_Dst_window = min(omni_data['DST1800'].loc[Index_label_Dst[0]:Index_label_Dst[-1]])
+                        ax.axvline(omni_data[omni_data['DST1800']==min_Dst_window].index[0], label='Min(Dst)', color='red', alpha=0.7, linewidth=3, linestyle='--')
+                        
+                        dt_G2001_idxLabel = []
+                        for idx in range(len(Index_label_Dst)):
+                            dt_G2001_idxLabel.append(abs(arrival_datetime - Index_label_Dst[idx]))
+                        
+                        # at the index of the min diff betn t_G2001 and t_min(Dst) within the window 
+                        ax.axvline(Index_label_Dst[dt_G2001_idxLabel.index(min(dt_G2001_idxLabel))], label='Min(dt)', color='black', alpha=0.7, linewidth=3, linestyle='--')     
+                
+                        T_RED = omni_data[omni_data['DST1800']==min_Dst_window].index[0]
+                        T_BLACK = Index_label_Dst[dt_G2001_idxLabel.index(min(dt_G2001_idxLabel))]
+                        T_GREEN = arrival_datetime
+                        
+                        AVG = Timestamp((T_RED.value + T_BLACK.value + T_GREEN.value)/3.0)
+                        
+                        ax.axvline(AVG, label='AVG_t', color='brown', alpha=0.7, linewidth=3, linestyle='--')
+                        
+                        ax.legend(loc='upper right', frameon=False, prop={'size': 10})
+                        ax.set_xlim([omni_data.index[0], omni_data.index[-1]])
+                        
+                        plt.xlabel('Date')
+                        fig.tight_layout()
+                        
+                        st = str(omni_data.index[0].year)+str(omni_data.index[0].month)+str(omni_data.index[0].day)+str(omni_data.index[0].hour)+str(omni_data.index[0].minute)+str(omni_data.index[0].second)
+                        en = str(omni_data.index[-1].year)+str(omni_data.index[-1].month)+str(omni_data.index[-1].day)+str(omni_data.index[-1].hour)+str(omni_data.index[-1].minute)+str(omni_data.index[-1].second)
+                        plt.savefig(os.path.join(save_path, 'Output_plots' + '/', 'OMNI_Data_for_CME_No_'+str(event_num)+'_'+st+'-'+en+'.png'))
+                                            
+                        # APPEND THE OUTPUT TRANSIT TIME WITH THE CME INFO 
+                        est_trans_time = Index_label_Dst[dt_G2001_idxLabel.index(min(dt_G2001_idxLabel))] - sample.index[event_num]
+                        
+                        tran_time_hours = (est_trans_time.components.days * 24) + (est_trans_time.components.minutes / 60) + (est_trans_time.components.seconds / 3600)
+                        
+                        final_table = final_table.append({'CME_datetime': sample.index[event_num], 
+                                                          'Width': sample['Width'][event_num], 
+                                                          'Linear_Speed': sample['Linear_Speed'][event_num], 
+                                                          'Initial_Speed': sample['Initial_Speed'][event_num], 
+                                                          'Final_Speed': sample['Final_Speed'][event_num], 
+                                                          'Speed_20Rs': sample['Speed_20Rs'][event_num], 
+                                                          'Accel': sample['Accel'][event_num], 
+                                                          'MPA': sample['MPA'][event_num], 
+                                                          'Transit_time_hrs': tran_time_hours, 
+                                                          'est_ICME_datetime': est_trans_time}, ignore_index=True)
+                        
+                        Index_label_Dst, min_Dst_window = [], []
+
+
+        else:
+            print('The OMNI data from '+str(start_datetime)+' to '+str(end_datetime)+' has no Dst value below '+str(threshold)+' nT.')
+            print('-------------------------------------------------------\n')
 
 # In[]: PLOT SPEED VS TRANSIT TIME 
 plt.figure()

6418d2e59ae72177579b65c68ef5ea99e3a61fb4
MohamedNedal <mohamed.nedal.solar@gmail.com>
2021-01-05 23:40:36 +0200

    Main code

--- plot_omni_CME-ICME_matching.py
+++ plot_omni_CME-ICME_matching.py
@@ -7,12 +7,11 @@ OMNI provides interspersed data from various spacecrafts.
 There are 55 variables provided in the OMNI Data Import.
 """
 import numpy as np
-from pandas import read_excel, to_numeric
+from pandas import read_excel, DataFrame
 from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
-import heliopy.data.omni as omni
 import os.path
-from necessary_functions import get_omni, magnetic_conversion, G2001
+from necessary_functions import get_omni, G2001
 import warnings
 warnings.filterwarnings('ignore')
 
@@ -22,6 +21,247 @@ try:
     os.mkdir(save_path + 'Output_plots')
 except OSError as error:
     print(error)
+
+# In[]: IMPORT THE LIST OF RANDOM CME EVENTS 
+sample = read_excel('Random_40_CMEs.xlsx', index_col='Datetime')
+
+# Create an empty table to be filled with the CME info and its estimated transit time 
+final_table = []
+
+# In[]: --- Try to build a JSON file structure with these info for all events --- 
+print('Predicting the transit time of the CMEs using G2001 model .. \n')
+
+event_num = 4
+arrival_datetime = G2001(sample.index[event_num], sample.Linear_Speed[event_num])
+
+dt = arrival_datetime - sample.index[event_num]
+print('CME number:', event_num+1)
+print('Event launched at:', sample.index[event_num])
+print('Arrived at:', arrival_datetime)
+print('Estimated Transit time: {} days, {} hours, {} minutes' .format(dt.components.days, 
+                                                                      dt.components.hours, 
+                                                                      dt.components.minutes))
+print('with mean error of:', 11.04, ' hours')
+print('-------------------------------------------------------')
+
+# In[]: --- 
+start_window = arrival_datetime - timedelta(hours=11.04)
+end_window = arrival_datetime + timedelta(hours=11.04)
+
+start_datetime = datetime(start_window.year,
+                          start_window.month,
+                          start_window.day,
+                          start_window.hour,
+                          start_window.minute,
+                          start_window.second)
+
+end_datetime = datetime(end_window.year,
+                        end_window.month,
+                        end_window.day,
+                        end_window.hour,
+                        end_window.minute,
+                        end_window.second)
+
+omni_data = get_omni(start_datetime, end_datetime)
+
+# In[]: --- 
+fig, axs = plt.subplots(8, 1, figsize=(15,15), dpi=300, sharex=True)
+
+axs[0].plot(omni_data['F1800'], color='blue', label='$B_t$')
+axs[0].plot(omni_data['BZ_GSE1800'], color='red', label='$B_z$')
+axs[0].set_ylabel('B (nT)')
+
+axs[1].plot(omni_data['THETA_AV1800'], label=r'$\theta$')
+axs[1].plot(omni_data['PHI_AV1800'], label=r'$\phi$')
+axs[1].set_ylabel('Angle (deg)')
+
+axs[2].plot(omni_data['BX_GSE1800'], label=r'$B_x$ $GSE$')
+axs[2].plot(omni_data['BY_GSE1800'], label=r'$B_y$ $GSE$')
+axs[2].set_ylabel('B (nT)')
+
+axs[3].plot(omni_data['V1800'], label='Flow Speed')
+axs[3].set_ylabel(r'$V$ $(km.s^{-1})$')
+
+axs[4].plot(omni_data['N1800'], label='Proton Density')
+axs[4].set_ylabel(r'$n_{p}$ $(cm^{-3})$')
+
+axs[5].plot(omni_data['Pressure1800'], label='Dynamic Pressure')
+axs[5].set_ylabel('P (nPa)')
+
+axs[6].plot(omni_data['Beta1800'], label='Plasma Beta')
+axs[6].set_ylabel('Beta')
+
+axs[7].plot(omni_data['DST1800'], label='Dst index')
+axs[7].set_ylabel('Dst (nT)')
+
+# Assign a threshold of the Dst (nT) to look for geomagnetic storms 
+offset = 5
+threshold = np.average(omni_data['DST1800'] - offset)
+# Select all the rows which satisfies the criteria 
+# Convert the collection of index labels to list 
+Index_label_Dst = omni_data[omni_data['DST1800'] <= threshold].index.tolist()
+
+print('\nDefine timestamps where Dst < ', round(threshold,2), 'nT')
+
+# Find Geomagnetic Storms in Data 
+for i in range(1, len(omni_data)):
+    if omni_data['DST1800'][i] < threshold:
+
+        axs[0].axvline(omni_data['F1800'].index[i], color='tomato', alpha=0.1, linewidth=1, linestyle='--')
+        axs[0].axvspan(omni_data['F1800'].index[i-1], omni_data['F1800'].index[i], facecolor='#FFCC66', alpha=0.5)
+
+        axs[1].axvline(omni_data['THETA_AV1800'].index[i], color='tomato', alpha=0.1, linewidth=1, linestyle='--')
+        axs[1].axvspan(omni_data['THETA_AV1800'].index[i-1], omni_data['THETA_AV1800'].index[i], facecolor='#FFCC66', alpha=0.5)
+        
+        axs[2].axvline(omni_data['BX_GSE1800'].index[i], color='tomato', alpha=0.1, linewidth=1, linestyle='--')
+        axs[2].axvspan(omni_data['BX_GSE1800'].index[i-1], omni_data['BX_GSE1800'].index[i], facecolor='#FFCC66', alpha=0.5)
+        
+        axs[3].axvline(omni_data['V1800'].index[i], color='tomato', alpha=0.1, linewidth=1, linestyle='--')
+        axs[3].axvspan(omni_data['V1800'].index[i-1], omni_data['V1800'].index[i], facecolor='#FFCC66', alpha=0.5)
+
+        axs[4].axvline(omni_data['N1800'].index[i], color='tomato', alpha=0.1, linewidth=1, linestyle='--')
+        axs[4].axvspan(omni_data['N1800'].index[i-1], omni_data['N1800'].index[i], facecolor='#FFCC66', alpha=0.5)
+
+        axs[5].axvline(omni_data['Pressure1800'].index[i], color='tomato', alpha=0.1, linewidth=1, linestyle='--')
+        axs[5].axvspan(omni_data['Pressure1800'].index[i-1], omni_data['Pressure1800'].index[i], facecolor='#FFCC66', alpha=0.5)
+
+        axs[6].axvline(omni_data['Beta1800'].index[i], color='tomato', alpha=0.1, linewidth=1, linestyle='--')
+        axs[6].axvspan(omni_data['Beta1800'].index[i-1], omni_data['Beta1800'].index[i], facecolor='#FFCC66', alpha=0.5)        
+        
+        axs[7].axvline(omni_data['DST1800'].index[i], color='tomato', alpha=0.1, linewidth=1, linestyle='--')
+        axs[7].axvspan(omni_data['DST1800'].index[i-1], omni_data['DST1800'].index[i], facecolor='#FFCC66', alpha=0.5)
+
+''' 
+ RUN A FOR-LOOP THAT CHECK THE MIN. DIFF. BETWEEN 
+ 'arrival_datetime' & 'Index_label_<ANY_SW_PARAM.>' 
+ & STORE THE 'idx' VALUES FOR EACH SW PARAM. AS A LIST 
+ FINALLY, TAKE THE AVG. OF THAT LIST, TO BE 
+ THE PROPABLE ARRIVAL TIME OF THE EVENT. 
+ 
+ ''' 
+
+for ax in axs:
+    # estimated travel time from G2001 model 
+    ax.axvline(arrival_datetime, label='G2001', color='green', alpha=0.7, linewidth=3, linestyle='--')
+    
+    # Find the local min Dst value within the window of 'Index_label_Dst' 
+    try: 
+        if Index_label_Dst == []:
+            print('Index_label_Dst is empty')
+        else:            
+            min_Dst_window = min(omni_data['DST1800'].loc[Index_label_Dst[0]:Index_label_Dst[-1]])
+    except IndexError as idx_err:
+        print(idx_err)
+    
+    # at min value of Dst index 
+    # try:
+    #     ax.axvline(all(omni_data[omni_data['DST1800']==min_Dst_window].index), label='Min(Dst)', color='red', alpha=0.7, linewidth=3, linestyle='--')
+    # except ValueError as valerr:
+    #     print(valerr)
+    ax.axvline(omni_data[omni_data['DST1800']==min_Dst_window].index[0], label='Min(Dst)', color='red', alpha=0.7, linewidth=3, linestyle='--')
+    
+    dt_G2001_idxLabel = []
+    for idx in range(len(Index_label_Dst)):
+        dt_G2001_idxLabel.append(abs(arrival_datetime - Index_label_Dst[idx]))
+    
+    # at the index of the min diff betn t_G2001 and t_min(Dst) within the window 
+    ax.axvline(Index_label_Dst[dt_G2001_idxLabel.index(min(dt_G2001_idxLabel))], label='Min(dt)', color='black', alpha=0.7, linewidth=3, linestyle='--')
+    
+    
+    T_RED = omni_data[omni_data['DST1800']==min_Dst_window].index[0]
+    T_BLACK = Index_label_Dst[dt_G2001_idxLabel.index(min(dt_G2001_idxLabel))]
+    T_GREEN = arrival_datetime
+    
+    from pandas import Timestamp
+    AVG = Timestamp((T_RED.value + T_BLACK.value + T_GREEN.value)/3.0)
+    
+    ax.axvline(AVG, label='AVG_t', color='brown', alpha=0.7, linewidth=3, linestyle='--')
+    
+    ax.legend(loc='upper right', frameon=False, prop={'size': 10})
+    ax.set_xlim([omni_data.index[0], omni_data.index[-1]])
+
+plt.xlabel('Date')
+fig.tight_layout()
+
+# ============================================= 
+st = str(omni_data.index[0].year)+str(omni_data.index[0].month)+str(omni_data.index[0].day)+str(omni_data.index[0].hour)+str(omni_data.index[0].minute)+str(omni_data.index[0].second)
+en = str(omni_data.index[-1].year)+str(omni_data.index[-1].month)+str(omni_data.index[-1].day)+str(omni_data.index[-1].hour)+str(omni_data.index[-1].minute)+str(omni_data.index[-1].second)
+plt.savefig(os.path.join(save_path, 'Output_plots' + '/', 'OMNI_Data_for_CME_No_'+str(event_num)+'_'+st+'-'+en+'.png'))
+
+print('\nNaNs?', omni_data.isnull().values.any())
+
+# APPEND THE OUTPUT TRANSIT TIME WITH THE CME INFO 
+est_trans_time = Index_label_Dst[dt_G2001_idxLabel.index(min(dt_G2001_idxLabel))] - sample.index[event_num]
+
+tran_time_hours = (est_trans_time.components.days * 24) + (est_trans_time.components.minutes / 60) + (est_trans_time.components.seconds / 3600)
+
+# In[]: FINALIZE THE OUTPUT 
+cols = sample.columns
+
+cols = cols.insert(0, 'CME_datetime')
+cols = cols.insert(len(cols)+1, 'Transit_time_hrs')
+cols = cols.insert(len(cols)+1, 'est_ICME_datetime')
+
+final_table = DataFrame(columns=cols)
+
+final_table = final_table.append({'CME_datetime': sample.index[event_num], 
+                                  'Width': sample['Width'][event_num], 
+                                  'Linear_Speed': sample['Linear_Speed'][event_num], 
+                                  'Initial_Speed': sample['Initial_Speed'][event_num], 
+                                  'Final_Speed': sample['Final_Speed'][event_num], 
+                                  'Speed_20Rs': sample['Speed_20Rs'][event_num], 
+                                  'Accel': sample['Accel'][event_num], 
+                                  'MPA': sample['MPA'][event_num], 
+                                  'Transit_time_hrs': tran_time_hours, 
+                                  'est_ICME_datetime': est_trans_time}, ignore_index=True)
+
+# In[] --- 
+
+
+
+
+# In[] --- 
+
+
+
+
+# In[] --- 
+
+
+
+
+
+
+
+
+# In[] --- 
+
+
+
+
+
+
+
+
+# In[]: --- 
+# Days = est_trans_time.days
+# Seconds = est_trans_time.seconds
+
+# Hours = Seconds//3600 # // to get only the integer part 
+# Minutes = (Seconds//60)%60 # to get the floar of the rest 
+
+# # 1 day == 24 hours == 1440 minutes == 86400 seconds 
+# # hours = (hours/day) + (hours/minutes) + (hours/seconds) 
+# H = (Days*24) + (Minutes/60) + (Seconds/3600)
+
+# print(round(H,2))
+
+# In[] --- 
+
+# from time import strftime
+
+# # NOTE: The following resets if it goes over 23:59:59 
+# print(strftime('%H:%M:%S', gmtime(S)))
 
 # In[]: Define the duration (year, month day, hour, minute, second) 
 # starttime = datetime(2013, 1, 17, 0, 0, 0)
@@ -98,231 +338,20 @@ except OSError as error:
 
 # sample.to_excel('Random_'+str(num_rand_samples)+'_CMEs.xlsx') # already done 
 
-# In[]: --- 
-# ======================== INTEGRATE CME WITH OMNI DATA ============================== 
-
-# In[]: IMPORT THE LIST OF RANDOM CME EVENTS 
-sample = read_excel('Random_40_CMEs.xlsx', index_col='Datetime')
-
-# In[]: --- Try to build a JSON file structure with these info for all events --- 
-print('Predicting the transit time of the CME using G2001 model .. \n')
-
-event_num = 2
-arrival_datetime = G2001(sample.index[event_num], sample.Linear_Speed[event_num])
-
-dt = arrival_datetime - sample.index[event_num]
-print('CME number:', event_num+1)
-print('Event launched at:', sample.index[event_num])
-print('Arrived at:', arrival_datetime)
-print('Estimated Transit time: {} days, {} hours, {} minutes' .format(dt.components.days, 
-                                                                      dt.components.hours, 
-                                                                      dt.components.minutes))
-print('with mean error of:', 11.04, ' hours')
-print('-------------------------------------------------------')
-
-# In[]: --- 
-start_window = arrival_datetime - timedelta(hours=11.04)
-end_window = arrival_datetime + timedelta(hours=11.04)
-
-start_datetime = datetime(start_window.year,
-                          start_window.month,
-                          start_window.day,
-                          start_window.hour,
-                          start_window.minute,
-                          start_window.second)
-
-end_datetime = datetime(end_window.year,
-                        end_window.month,
-                        end_window.day,
-                        end_window.hour,
-                        end_window.minute,
-                        end_window.second)
-
-omni_data = get_omni(start_datetime, end_datetime)
-
 # In[]: --- PLOT THE SIGMA VALUE --- 
-plt.figure(figsize=(15,3))
-plt.plot(omni_data.index, omni_data['ABS_B1800'], label='B_abs')
+# plt.figure(figsize=(15,3))
+# plt.plot(omni_data.index, omni_data['ABS_B1800'], label='B_abs')
 
-plt.fill_between(omni_data.index, 
-                 omni_data['ABS_B1800']-omni_data['SIGMA$ABS_B1800'], 
-                 omni_data['ABS_B1800']+omni_data['SIGMA$ABS_B1800'], 
-                 facecolor='grey', alpha=0.15, label='Sigma')
+# plt.fill_between(omni_data.index, 
+#                  omni_data['ABS_B1800']-omni_data['SIGMA$ABS_B1800'], 
+#                  omni_data['ABS_B1800']+omni_data['SIGMA$ABS_B1800'], 
+#                  facecolor='grey', alpha=0.15, label='Sigma')
 
-plt.xlabel('Datetime')
-plt.ylabel(r'$B_{ABS}$ $(nT)$')
-plt.legend(frameon=False)
-plt.xlim([omni_data.index[0], omni_data.index[-1]])
-plt.show()
-
-# In[]: --- 
-# Assign a threshold of the Dst (nT) to look for geomagnetic storms 
-offset = 5
-threshold = np.average(omni_data['DST1800'] - offset)
-
-print('\nDefine timestamps where Dst <', threshold, 'nT')
-
-# Select all the rows which satisfies the criteria 
-# convert the collection of index labels to list 
-Index_label_Dst = omni_data[omni_data['DST1800'] <= threshold].index.tolist()
-
-fig, ax = plt.subplots(1, 1, figsize=(15,3), dpi=300)
-ax.plot(omni_data['DST1800'], label='DST')
-
-# Find Geomagnetic Storms in Data 
-for i in range(1, len(omni_data)):
-    if omni_data['DST1800'][i] <= threshold:
-        ax.axvline(omni_data['DST1800'].index[i], color='tomato', alpha=0.1, linewidth=1, linestyle='--')
-        ax.axvspan(omni_data['DST1800'].index[i-1], omni_data['DST1800'].index[i], facecolor='#FFCC66', alpha=0.7)
-
-ax.axvline(arrival_datetime, label='G2001', color='green', alpha=0.7, linewidth=3, linestyle='--')
-
-# Find the local min Dst value within the window of 'Index_label_Dst' 
-min_Dst_window = min(omni_data['DST1800'].loc[Index_label_Dst[0]:Index_label_Dst[-1]])
-
-ax.axvline(omni_data[omni_data['DST1800']==min_Dst_window].index, 
-            label='Min(Dst)', color='red', alpha=0.7, linewidth=3, linestyle='--')
-
-ax.legend(loc='upper right', frameon=False, prop={'size': 10})
-ax.set_xlim([omni_data.index[0], omni_data.index[-1]])
-plt.xlabel('Date')
-plt.ylabel('Dst (nT)')
-fig.tight_layout()
-
-print('\nNaNs?', omni_data.isnull().values.any())
-
-# In[]: --- >>> APPLY STEP NO. 6 FROM MY NOTEBOOK <<< 
-# ===================================================== 
-
-fig, axs = plt.subplots(7, 1, figsize=(15,10), dpi=300, sharex=True)
-
-axs[0].plot(omni_data['F'], label='Avg Mag Field')
-axs[0].set_ylabel('Bt (nT)')
-
-axs[1].plot(omni_data['BX_GSE'], label='Bx GSE')
-axs[1].plot(omni_data['BY_GSM'], label='By GSM')
-axs[1].plot(omni_data['BZ_GSM'], label='Bz GSM')
-axs[1].set_ylabel('nT')
-
-axs[2].plot(omni_data['flow_speed'], label='Flow Speed')
-axs[2].set_ylabel('Flow Speed\n(km/s)')
-
-axs[3].plot(omni_data['proton_density'], label='Proton Density')
-axs[3].set_ylabel('Proton Density\n(#/cm**3)')
-
-axs[4].plot(omni_data['Pressure'], label='Pressure')
-axs[4].set_ylabel('Pressure\n(nPa)')
-
-axs[5].plot(omni_data['Beta'], label='Beta')
-axs[5].set_ylabel('Plasma\nBeta')
-
-axs[6].plot(omni_data['SYM_H'], label='SYM-H')
-axs[6].set_ylabel('Dst (nT)')
-
-# Find Geomagnetic Storms in Data --- ADJUST THE COLUMNS' LABELS --- <<< 
-for i in range(1, len(omni_data)):
-    if omni_data['SYM_H'][i] < threshold:
-
-        axs[0].axvline(omni_data['F'].index[i], color='tomato', alpha=0.1, linewidth=1, linestyle='--')
-        axs[0].axvspan(omni_data['F'].index[i-1], omni_data['F'].index[i], facecolor='#FFCC66', alpha=0.7)
-        
-        axs[1].axvline(omni_data['BX_GSE'].index[i], color='tomato', alpha=0.1, linewidth=1, linestyle='--')
-        axs[1].axvspan(omni_data['BX_GSE'].index[i-1], omni_data['BX_GSE'].index[i], facecolor='#FFCC66', alpha=0.7)
-        
-        axs[2].axvline(omni_data['flow_speed'].index[i], color='tomato', alpha=0.1, linewidth=1, linestyle='--')
-        axs[2].axvspan(omni_data['flow_speed'].index[i-1], omni_data['flow_speed'].index[i], facecolor='#FFCC66', alpha=0.7)
-
-        axs[3].axvline(omni_data['proton_density'].index[i], color='tomato', alpha=0.1, linewidth=1, linestyle='--')
-        axs[3].axvspan(omni_data['proton_density'].index[i-1], omni_data['proton_density'].index[i], facecolor='#FFCC66', alpha=0.7)
-
-        axs[4].axvline(omni_data['Pressure'].index[i], color='tomato', alpha=0.1, linewidth=1, linestyle='--')
-        axs[4].axvspan(omni_data['Pressure'].index[i-1], omni_data['Pressure'].index[i], facecolor='#FFCC66', alpha=0.7)
-
-        axs[5].axvline(omni_data['Beta'].index[i], color='tomato', alpha=0.1, linewidth=1, linestyle='--')
-        axs[5].axvspan(omni_data['Beta'].index[i-1], omni_data['Beta'].index[i], facecolor='#FFCC66', alpha=0.7)        
-        
-        axs[6].axvline(omni_data['SYM_H'].index[i], color='tomato', alpha=0.1, linewidth=1, linestyle='--')
-        axs[6].axvspan(omni_data['SYM_H'].index[i-1], omni_data['SYM_H'].index[i], facecolor='#FFCC66', alpha=0.7)
-
-for ax in axs:
-    ax.axvline(arrival_datetime, label='G2001', color='black', alpha=0.7, linewidth=3, linestyle='--')
-    ax.legend(loc='upper right', frameon=False, prop={'size': 10})
-    ax.set_xlim([omni_data.index[0], omni_data.index[-1]])
-plt.xlabel('Date')
-fig.tight_layout()
+# plt.xlabel('Datetime')
+# plt.ylabel(r'$B_{ABS}$ $(nT)$')
+# plt.legend(frameon=False)
+# plt.xlim([omni_data.index[0], omni_data.index[-1]])
 # plt.show()
-
-# ========================================== 
-st = str(omni_data.index[0].year)+str(omni_data.index[0].month)+str(omni_data.index[0].day)+str(omni_data.index[0].hour)+str(omni_data.index[0].minute)+str(omni_data.index[0].second)
-en = str(omni_data.index[-1].year)+str(omni_data.index[-1].month)+str(omni_data.index[-1].day)+str(omni_data.index[-1].hour)+str(omni_data.index[-1].minute)+str(omni_data.index[-1].second)
-plt.savefig(os.path.join(save_path, 'Output_plots' + '/', 'OMNI_Data_'+st+'--'+en+'.png'))
-
-# In[] --- 
-''' 
- RUN A FOR-LOOP THAT CHECK THE MIN. DIFF. 
- BETWEEN 'arrival_datetime' & 'Index_label_<ANY_SW_PARAM.>' 
- & STORE THE 'idx' VALUES FOR EACH SW PARAM. AS A LIST 
- & FINALLY, TAKE THE AVG. OF THAT LIST, TO BE 
- THE PROPABLE ARRIVAL TIME OF THE EVENT. 
- ''' 
-
-[val, idx] = min(abs(arrival_datetime - Index_label_Dst))
-
-
-
-# In[] --- 
-
-
-
-
-
-
-# In[] --- 
-
-
-
-
-
-# In[]: --- 
-
-
-# In[] --- 
-
-
-
-
-# In[] --- 
-
-
-
-
-# In[] --- 
-
-
-
-
-
-
-
-
-# In[] --- 
-
-
-
-
-
-
-# In[] --- 
-
-
-
-
-# In[] --- 
-
-
-
-
-
 
 # In[]: --- 
 # fig, ax = plt.subplots(1, 1, figsize=(15,3), dpi=300, sharex=True)

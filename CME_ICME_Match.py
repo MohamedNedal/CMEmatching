2f8b2981ae5f37f684864bee874135533753ae4e
MohamedNedal <mohamed.nedal.solar@gmail.com>
2021-01-11 00:56:48 +0200

    Fixing bugs

--- CME_ICME_Match.py
+++ CME_ICME_Match.py
@@ -5,7 +5,6 @@ Plotting OMNI Data
 
 Importing and plotting data from OMNI Web Interface.
 OMNI provides interspersed data from various spacecrafts.
-There are 55 variables provided in the OMNI Data Import. 
 
 """
 # import numpy as np
@@ -14,6 +13,7 @@ from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 import os.path
 from necessary_functions import get_omni, G2001
+from statistics import mean
 import warnings
 warnings.filterwarnings('ignore')
 
@@ -37,9 +37,6 @@ print('\nPredicting the transit time of the CMEs using the G2001 model\nwith mea
 print('For more info about the G2001 model, check this paper:\nGopalswamy, N., Lara, A., Yashiro, S., Kaiser, M. L., and Howard,\nR. A.: Predicting the 1-AU arrival times of coronal mass ejections,\nJ. Geophys. Res., 106, 29 207, 2001a.\n')
 print('And this paper:\nOwens, M., & Cargill, P. (2004, January).\nPredictions of the arrival time of Coronal Mass Ejections at 1AU:\nan analysis of the causes of errors.\nIn Annales Geophysicae (Vol. 22, No. 2, pp. 661-671). Copernicus GmbH.\n')
 print('=========================================================')
-
-# In[]: Drop rows (CME events) that have problems with their respective OMNI data 
-# sample.drop([sample.index[4], sample.index[14]], inplace=True)
 
 # In[]: --- 
 # FINALIZE THE OUTPUT 
@@ -110,31 +107,37 @@ for event_num in range(len(sample)):
             
             fig.suptitle('For CME event that is launched on: '+str(sample.index[event_num]))
             
-            axs[0].plot(omni_data['F1800'], color='blue', label='$B_t$')
-            axs[0].plot(omni_data['BZ_GSE1800'], color='red', label='$B_z$')
-            axs[0].set_ylabel('B (nT)')
+            axs[0].plot(omni_data['F1800'])
+            axs[0].set_ylabel('r$B_t$ $(nT)$')
             
-            axs[1].plot(omni_data['THETA_AV1800'], label=r'$\theta$')
-            axs[1].plot(omni_data['PHI_AV1800'], label=r'$\phi$')
-            axs[1].set_ylabel('Angle (deg)')
+            axs[1].plot(omni_data['BX_GSE1800'], label=r'$B_x$ $GSE$')
+            axs[1].plot(omni_data['BY_GSE1800'], label=r'$B_y$ $GSE$')
+            axs[1].plot(omni_data['BZ_GSE1800'], color='red', label='r$B_z$ $GSE$')
+            axs[1].set_ylabel(r'$B_{x,y,z}\;(nT)$')
             
-            axs[2].plot(omni_data['BX_GSE1800'], label=r'$B_x$ $GSE$')
-            axs[2].plot(omni_data['BY_GSE1800'], label=r'$B_y$ $GSE$')
-            axs[2].set_ylabel('B (nT)')
+            axs[2].plot(omni_data['V1800'], label='$V_{sw}$')
+            axs[2].set_ylabel(r'$V_{sw}$ $(km.s^{-1})$')
             
-            axs[3].plot(omni_data['V1800'], label='Flow Speed')
-            axs[3].set_ylabel(r'$V$ $(km.s^{-1})$')
+            axs[3].plot(omni_data['T1800'], label='$T_p$')
+            # Calculating half the expected solar wind temperature (0.5Texp) 
+            if mean(omni_data['V1800']) >= 500:
+                Texp = 0.5 * (((0.031*omni_data['V1800']) - 5.1)**2) * (10**3)
+            else:
+                Texp = ((0.0106*omni_data['V1800']) - 0.278)**2
+            Texp.rename('Texp', inplace=True)
+            axs[3].plot(Texp, label='$0.5T_{exp}$')
+            axs[3].set_ylabel('T (K)')
             
-            axs[4].plot(omni_data['N1800'], label='Proton Density')
-            axs[4].set_ylabel(r'$n_{p}$ $(cm^{-3})$')
+            axs[4].plot(omni_data['Ratio1800'])
+            axs[4].set_ylabel('$\dfrac{N_{alpha}}{N_{p}}$')
             
-            axs[5].plot(omni_data['Pressure1800'], label='Dynamic Pressure')
-            axs[5].set_ylabel('P (nPa)')
+            axs[5].plot(omni_data['N1800'], label=r'$n_{p}$ $(cm^{-3})$')
+            axs[5].plot(omni_data['Pressure1800'], label='P (nPa)')
             
-            axs[6].plot(omni_data['Beta1800'], label='Plasma Beta')
-            axs[6].set_ylabel('Beta')
+            axs[6].plot(omni_data['Beta1800'])
+            axs[6].set_ylabel(r'$Plasma\;\beta$')
             
-            axs[7].plot(omni_data['DST1800'], label='Dst index')
+            axs[7].plot(omni_data['DST1800'])
             axs[7].set_ylabel('Dst (nT)')
 
             # Find Geomagnetic Storms in Data 
@@ -143,14 +146,21 @@ for event_num in range(len(sample)):
                 if omni_data['DST1800'][i] <= threshold:
                     
                     axs[0].axvspan(omni_data['F1800'].index[i-1], omni_data['F1800'].index[i], facecolor='#FFCC66', alpha=0.5)
-                    axs[1].axvspan(omni_data['THETA_AV1800'].index[i-1], omni_data['THETA_AV1800'].index[i], facecolor='#FFCC66', alpha=0.5)
-                    axs[2].axvspan(omni_data['BX_GSE1800'].index[i-1], omni_data['BX_GSE1800'].index[i], facecolor='#FFCC66', alpha=0.5)
-                    axs[3].axvspan(omni_data['V1800'].index[i-1], omni_data['V1800'].index[i], facecolor='#FFCC66', alpha=0.5)
-                    axs[4].axvspan(omni_data['N1800'].index[i-1], omni_data['N1800'].index[i], facecolor='#FFCC66', alpha=0.5)
-                    axs[5].axvspan(omni_data['Pressure1800'].index[i-1], omni_data['Pressure1800'].index[i], facecolor='#FFCC66', alpha=0.5)
+                    axs[1].axvspan(omni_data['BX_GSE1800'].index[i-1], omni_data['THETA_AV1800'].index[i], facecolor='#FFCC66', alpha=0.5)
+                    axs[2].axvspan(omni_data['V1800'].index[i-1], omni_data['BX_GSE1800'].index[i], facecolor='#FFCC66', alpha=0.5)
+                    axs[3].axvspan(omni_data['T1800'].index[i-1], omni_data['V1800'].index[i], facecolor='#FFCC66', alpha=0.5)
+                    axs[4].axvspan(omni_data['Ratio1800'].index[i-1], omni_data['N1800'].index[i], facecolor='#FFCC66', alpha=0.5)
+                    axs[5].axvspan(omni_data['N1800'].index[i-1], omni_data['Pressure1800'].index[i], facecolor='#FFCC66', alpha=0.5)
                     axs[6].axvspan(omni_data['Beta1800'].index[i-1], omni_data['Beta1800'].index[i], facecolor='#FFCC66', alpha=0.5)
                     axs[7].axvspan(omni_data['DST1800'].index[i-1], omni_data['DST1800'].index[i], facecolor='#FFCC66', alpha=0.5)                    
     
+                    for ax in axs:
+                        ax.legend(loc='upper right', frameon=False, prop={'size': 10})
+                        ax.set_xlim([omni_data.index[0], omni_data.index[-1]])
+                        
+                        plt.xlabel('Date')
+                        fig.tight_layout()
+                        
                     ''' 
                      RUN A FOR-LOOP THAT CHECK THE MIN. DIFF. BETWEEN 
                      'arrival_datetime' & 'Index_label_<ANY_SW_PARAM.>' 
@@ -182,22 +192,12 @@ for event_num in range(len(sample)):
                         AVG = Timestamp((T_RED.value + T_BLACK.value + T_GREEN.value)/3.0)
                         
                         ax.axvline(AVG, label='AVG_t', color='brown', alpha=0.7, linewidth=3, linestyle='--')
-                        
-                        ax.legend(loc='upper right', frameon=False, prop={'size': 10})
-                        ax.set_xlim([omni_data.index[0], omni_data.index[-1]])
-                        
-                        plt.xlabel('Date')
-                        fig.tight_layout()
-                        
-            
+                                    
             st = str(omni_data.index[0].year)+str(omni_data.index[0].month)+str(omni_data.index[0].day)+str(omni_data.index[0].hour)+str(omni_data.index[0].minute)+str(omni_data.index[0].second)
             en = str(omni_data.index[-1].year)+str(omni_data.index[-1].month)+str(omni_data.index[-1].day)+str(omni_data.index[-1].hour)+str(omni_data.index[-1].minute)+str(omni_data.index[-1].second)
             plt.savefig(os.path.join(save_path, 'Output_plots' + '/', 'OMNI_Data_for_CME_No_'+str(event_num)+'_'+st+'-'+en+'.png'))
 
-
-
             # APPEND THE OUTPUT TRANSIT TIME WITH THE CME INFO 
-            # >>> WHY final_table HAS REPEATED ROWS FOR THE SAME EVENT? <<< 
             est_trans_time = Index_label_Dst[dt_G2001_idxLabel.index(min(dt_G2001_idxLabel))] - sample.index[event_num]
             tran_time_hours = (est_trans_time.components.days * 24) + (est_trans_time.components.minutes / 60) + (est_trans_time.components.seconds / 3600)
 
@@ -211,17 +211,10 @@ for event_num in range(len(sample)):
                                               'MPA': sample['MPA'][event_num], 
                                               'Transit_time_hrs': tran_time_hours, 
                                               'est_ICME_datetime': est_trans_time}, ignore_index=True)
-                                
-
 
         else:
             print('The OMNI data from '+str(start_datetime)+' to '+str(end_datetime)+' has no Dst value below '+str(threshold)+' nT.')
             print('-------------------------------------------------------\n')
-
-
-# dropping duplicate values 
-# final_table2 = final_table
-# final_table2.drop_duplicates(keep=False,inplace=True)
 
 print('Final Report:\n===============')
 print('Total number of CMEs:', len(sample))
